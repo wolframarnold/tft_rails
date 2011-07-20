8c53bce958c4c62256bb6ec83e56696babbff157
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-07-20 07:41:58 -0700

    Fix issue with routes in Chapter 07 Don't do fail fast ni rspec

--- lib/generators/chapter07/solutions/solutions_generator.rb
+++ lib/generators/chapter07/solutions/solutions_generator.rb
@@ -1,10 +1,18 @@
 module Chapter07
   module Generators
-    class FinishGenerator < Rails::Generators::Base
+    class SolutionsGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_app_tree
         directory(self.class.source_root, Rails.root)
+      end
+
+      def add_help_route
+
+        dest = File.join(Rails.root,'config','routes.rb')
+        insert_into_file(dest, :after => %r{/about.*:to.*pages#about.*$}) do
+          "\n  match '/help',    :to => 'pages#help'"
+        end
       end
 
       def remove_static_index_html_file
