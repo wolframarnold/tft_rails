71070b37de6c231c36daee536e6f1899fe7341c0
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-07-23 16:13:11 -0700

    Fix chapter 11.2 solutions

--- lib/generators/chapter11_2/solutions/solutions_generator.rb
+++ lib/generators/chapter11_2/solutions/solutions_generator.rb
@@ -1,6 +1,12 @@
 module Chapter11_2
   module Generators
     class SolutionsGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_app_tree
+        directory(self.class.source_root, Rails.root)
+      end
+
     end
   end
 end
